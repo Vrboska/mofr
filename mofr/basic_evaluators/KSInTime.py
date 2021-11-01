fb20277b049795a9e0721b150abb6653929eb407
Zubal <T862363F@techdata.com>
2021-11-01 19:08:53 +0100

    KSInTime table added

--- mofr/basic_evaluators/KSInTime.py
+++ mofr/basic_evaluators/KSInTime.py
@@ -80,8 +80,8 @@ class KSInTimeEvaluator(Evaluator):
       #plt.xlim(min(_x)-0.1,max(_x)+0.1)
       #plt.ylim(-0.01,1.03)
       plt.xlabel('Month')
-      plt.ylabel('LIFT')
-      plt.title(f'LIFT in time for target "{self.targets[0][0]}"')
+      plt.ylabel('KS')
+      plt.title(f'KS in time for target "{self.targets[0][0]}"')
       plt.legend(lines, labels) #, loc=(0, -.38), prop=dict(size=14)
 
       plt.show() 
@@ -89,22 +89,51 @@ class KSInTimeEvaluator(Evaluator):
       return self
 
     def get_table(self):
-        pass
-
         
-        """
-        The idea is to have a table corresponding to the data shown in graph in a following format (or similar):
-                                
-                        KS on 'name of the target column'
-        Time             Model1         Model2      Model3       
-        ------------------------------------------------------------
-        202001          0.23            0.25           ...
-        202002          0.33            ...            ...
-        202003          0.54            ...            ...
-        ------------------------------------------------------------
-        All             0.42            ...            ...
+      """
+      The idea is to have a table corresponding to the data shown in graph in a following format (or similar):
+                              
+                  KS on 'name of the target column'
+      Time             Model1         Model2      Model3       
+      ------------------------------------------------------------
+      202001          0.23            0.25           ...
+      202002          0.33            ...            ...
+      202003          0.54            ...            ...
+      ------------------------------------------------------------
+      All             0.42            ...            ...
 
 
-        Let's implement this using pandas.pivot_table since it will make it easier later to combine different
-        tables into one.
-        """
+      Let's implement this using pandas.pivot_table since it will make it easier later to combine different
+      tables into one.
+      """
+      def ks_zipped(x):
+            """Auxilliary function for calculating Lift in pivot table."""
+            list_=list(zip(*x))
+            target_=list_[0]
+            score_=list_[1]  
+                        
+            return metrics.ks_score(target_, score_)
+            
+      n_scores=len(self.scores)
+
+      tables=[]
+
+      #creating pivot table for each score
+      for i in range(n_scores):          
+            target_=self.targets[0]
+            score_=self.scores[i]
+            df_=self.data[self.data[target_[1]]==1] #filtering for only target-observable cases
+
+            df_['target_score_']=list(zip(df_[target_[0]], df_[score_]))
+
+            pt=pd.pivot_table(df_, values=['target_score_'], index='month', columns=None, aggfunc=ks_zipped, fill_value=None, margins=True, dropna=True, margins_name='All')
+            pt.columns=[score_]
+            tables.append(pt)
+
+      #gathering the final table together
+      from functools import reduce
+      final_table=reduce(lambda x, y: x.merge(y,left_index=True,right_index=True), tables)
+      final_table=final_table.style.set_table_attributes("style='display:inline'").set_caption(f'KS on target "{target_[0]}"')  
+      self.table=final_table
+
+      return self
